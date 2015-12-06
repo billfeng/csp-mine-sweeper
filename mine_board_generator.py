19ba4d537e375ca33c2af72053a186553e2c4469
shangjoh <john.shang@mail.utoronto.ca>
2015-12-06 15:47:47 -0500

    fixed indexing bugs

--- mine_board_generator.py
+++ mine_board_generator.py
@@ -1,22 +1,21 @@
 import random
 
 def mine_board_generator(width, height):
-    mine_field = [width][height]
+    mine_field = [[0 for i in range(width)] for i in range(height)]
     for i in range(0, width):
         for j in range(0, height):
             # generate a random number here, and have a chance for the board to be set to "*"
             if (random.randint(0,5) == 0):
-                mine_field = "*"
+                mine_field[i][j] = "*"
 
-    mine_board = [width][height]
+    mine_board = [[0 for i in range(width)] for i in range(height)]
     for i in range(0, width):
         for j in range(0, height):
             if mine_field[i][j] != "*":
-                mine_board = 0
                 for k in range(-1, 2):
                     for l in range(-1, 2):
                         if 0 <= i + k < width and 0 <= j + l < height:
                             if mine_field[i + k][j + l] == "*":
-                                mine_board += 1
+                                mine_board[i][j] += 1
 
     return mine_board
