f5bac811b23395df94ae1e6a9f434b38d0f9dbd0
shangjoh <john.shang@mail.utoronto.ca>
2015-12-08 01:28:17 -0500

    added modifiable variables and 3d testing

--- minesweeper_sample_run.py
+++ minesweeper_sample_run.py
@@ -7,30 +7,46 @@ def print_sudo_soln(var_array):
         print([var.get_assigned_value() for var in row])
 
 if __name__ == "__main__":
+    
+    #the amount of tests to be run
     test_count = 5
-    while test_count > 0:
-        b = mine_board_generator_2d(9, 9)
-        print("Solving board " + 6 - test_count + ": ")
-        for row in b:
-            print(row)
-        print("Using 2d Model")
-        csp, var_array = minesweeper_csp_model_2d(b)
-        solver = BT(csp)
-        print("=======================================================")
-        print("GAC")
-        solver.bt_search(prop_GAC)
-        print("Solution")
-        print_sudo_soln(var_array)
-        test_count = test_count - 1
+    
+    #run 2d or 3d
+    dim = 2
+    
+    #board size
+    height = 9
+    width = 9
+    depth = 9
+    
+    if dim == 2:
+        while test_count > 0:
+            b = mine_board_generator_2d(height, width)
+            print("Solving board: ")
+            for row in b:
+                print(row)
+            print("Using 2d Model")
+            csp, var_array = minesweeper_csp_model_2d(b)
+            solver = BT(csp)
+            print("=======================================================")
+            print("GAC")
+            solver.bt_search(prop_GAC)
+            print("Solution")
+            print_sudo_soln(var_array)
+            test_count = test_count - 1
+    else:
+        while test_count > 0:
+            b = mine_board_generator_3d(height, width, depth)
+            print("Solving board: ")
+            for row in b:
+                print(row)
+            print("Using 3d Model")
+            csp, var_array = minesweeper_csp_model_3d(b)
+            solver = BT(csp)
+            print("=======================================================")
+            print("GAC")
+            solver.bt_search(prop_GAC)
+            print("Solution")
+            print_sudo_soln(var_array)
+            test_count = test_count - 1
 
-    '''
-    print("Using 3d Model")
-    csp, var_array = minesweeper_csp_model_3d(b2)
-    solver = BT(csp)
-    print("=======================================================")
-    print("GAC")
-    solver.bt_search(prop_GAC)
-    print("Solution")
-    print_sudo_soln(var_array)
-    print("=======================================================")
-    '''
