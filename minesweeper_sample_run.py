6f578827140ec6d56d04b05e78029c459e35cb43
shangjoh <john.shang@mail.utoronto.ca>
2015-12-08 01:21:38 -0500

    better testing

--- minesweeper_sample_run.py
+++ minesweeper_sample_run.py
@@ -2,26 +2,26 @@ from minesweeper_csp import *
 from propagators import *
 from mine_board_generator import *
 
-b1 = mine_board_generator_2d(9, 9)
-
-b2 = mine_board_generator_3d(9, 9, 9)
-
 def print_sudo_soln(var_array):
     for row in var_array:
         print([var.get_assigned_value() for var in row])
 
 if __name__ == "__main__":
-    print("Solving board b1:")
-    for row in b1:
-        print(row)
-    print("Using 2d Model")
-    csp, var_array = minesweeper_csp_model_2d(b1)
-    solver = BT(csp)
-    print("=======================================================")
-    print("GAC")
-    solver.bt_search(prop_GAC)
-    print("Solution")
-    print_sudo_soln(var_array)
+    test_count = 5
+    while test_count > 0:
+        b = mine_board_generator_2d(9, 9)
+        print("Solving board " + 6 - test_count + ": ")
+        for row in b:
+            print(row)
+        print("Using 2d Model")
+        csp, var_array = minesweeper_csp_model_2d(b)
+        solver = BT(csp)
+        print("=======================================================")
+        print("GAC")
+        solver.bt_search(prop_GAC)
+        print("Solution")
+        print_sudo_soln(var_array)
+        test_count = test_count - 1
 
     '''
     print("Using 3d Model")
