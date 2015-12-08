a24d25a7cd58aed74a798870f72121f60f70e13b
shangjoh <john.shang@mail.utoronto.ca>
2015-12-08 01:10:18 -0500

    added 3d board generator

--- mine_board_generator.py
+++ mine_board_generator.py
@@ -1,7 +1,7 @@
 import random
 
-def mine_board_generator(width, height):
-    mine_field = [[0 for i in range(width)] for i in range(height)]
+def mine_board_generator_2d(width, height):
+    mine_field = [[" " for i in range(width)] for i in range(height)]
     for i in range(0, width):
         for j in range(0, height):
             # generate a random number here, and have a chance for the board to be set to "*"
@@ -18,4 +18,27 @@ def mine_board_generator(width, height):
                             if mine_field[i + k][j + l] == "*":
                                 mine_board[i][j] += 1
 
-    return mine_field, mine_board
+    return mine_board, mine_field
+
+def mine_board_generator_3d(width, height, depth):
+    mine_field = [[[" " for i in range(width)] for i in range(height)] for i in range(depth)]
+    for i in range(0, width):
+        for j in range(0, height):
+            for k in range(0,depth):
+                # generate a random number here, and have a chance for the board to be set to "*"
+                if (random.randint(0,5) == 0):
+                    mine_field[i][j][k] = "*"
+
+    mine_board = [[[0 for i in range(width)] for i in range(height)] for i in range(depth)]
+    for i in range(0, width):
+        for j in range(0, height):
+            for k in range(0,depth):
+                if mine_field[i][j][k] != "*":
+                    for x in range(-1, 2):
+                        for y in range(-1, 2):
+                            for z in range(-1, 2):
+                                if 0 <= i + x < width and 0 <= j + y < height and 0 <= k + z < depth:
+                                    if mine_field[i + x][j + y][k + z] == "*":
+                                        mine_board[i][j][k] += 1
+
+    return mine_board, mine_field
